706784045a0c9d6819c233d00ce2d1d668395524
hkonsti <45428767+hkonsti@users.noreply.github.com>
2023-06-19 09:32:18 -0700

    configure template

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>Haven</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/havenhq/haven',
   },
-  chat: {
+  /*chat: {
     link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  },*/
+  docsRepositoryBase: 'https://github.com/havenhq/docs',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'With <3 from the Haven team.',
   },
 }
 
