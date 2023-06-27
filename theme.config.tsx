a54a6c1cafcee846dd9a1a1451c8982ed7ffb000
hkonsti <45428767+hkonsti@users.noreply.github.com>
2023-06-26 20:07:01 -0700

    Update docs

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Haven</span>,
-  project: {
-    link: 'https://github.com/havenhq/haven',
-  },
-  /*chat: {
+	logo: <span>Haven</span>,
+	project: {
+		link: "https://github.com/havenhq/haven",
+	},
+	/*chat: {
     link: 'https://discord.com',
   },*/
-  docsRepositoryBase: 'https://github.com/havenhq/docs',
-  footer: {
-    text: 'With <3 from the Haven team.',
-  },
-}
+	docsRepositoryBase: "https://github.com/havenhq/docs",
+	footer: {
+		text: "With <3 from Justus and Konsti.",
+	},
+};
 
-export default config
+export default config;
