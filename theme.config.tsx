4d7e4b09ba84b19a12ca547985ad9c0a5ad64b8f
hkonsti <45428767+hkonsti@users.noreply.github.com>
2023-07-01 20:07:14 -0700

    add discord

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-	logo: <span>Haven</span>,
+	logo: (
+		<span>
+			<b>Haven Documentation</b>
+		</span>
+	),
 	project: {
 		link: "https://github.com/havenhq/haven",
 	},
-	/*chat: {
-    link: 'https://discord.com',
-  },*/
+	chat: {
+		link: "https://discord.gg/JDjbfp6q2G",
+	},
 	docsRepositoryBase: "https://github.com/havenhq/docs",
 	footer: {
 		text: "With <3 from Justus and Konsti.",
